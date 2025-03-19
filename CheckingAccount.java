bde72a9e5d91f605c77ed4b948cdb44a68b32c2d
Langnir1 <joshua.halamka@bsu.edu>
2025-03-19 14:36:10 +0000

    CheckingAccount is working as intended. Could use some UI cleaning like spacing

--- CheckingAccount.java
+++ CheckingAccount.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class CheckingAccount implements hasMenu{
 	protected double balance;
 
@@ -24,10 +26,90 @@ public class CheckingAccount implements hasMenu{
 		"4) Withdraw";
 	}
 
+	public double getBalance(){
+                return balance;
+                //Change so that if the input is !double then returns 0
+        }
+
+        public String getBalanceString(){
+                return String.format("$%.02f", balance);
+        }
+
+        public void setBalance(double balance){
+                if(balance >= 0){
+                        this.balance = balance;
+                }
+                else{
+                        System.out.println("Negative is bad! Don't be in debt!");
+                }
+        }
+
 	public void start(){
+		Scanner input = new Scanner(System.in);
+		int choice;
+		boolean keepGoing = true;
+
 		System.out.println("You have entered your Checking Account!");
 		System.out.println();
-		System.out.println(menu());
+		
+		while(keepGoing){
+			System.out.println(menu());
+			System.out.println("What would you like to do?: ");
+			choice = input.nextInt();
+
+			if(choice == 1){
+				System.out.println("Thank you");
+				keepGoing = false;
+			} 
+			else if(choice == 2){
+				//System.out.println("Balance");
+				checkBalance();
+			}
+			else if(choice == 3){
+				//System.out.println("Deposit");
+				makeDeposit(input);
+			}
+			else if(choice == 4){
+				//System.out.println("Withdraw");
+				makeWithdraw(input);
+			}
+			else{
+				System.out.println("Invalid");
+			}
+		}
+
+	}
+	
+	public void checkBalance(){
+		System.out.println("Your balance is: " + getBalanceString());
+	}
+
+	public void makeDeposit(Scanner input){
+		System.out.println("How much would you like to deposit? ");
+		double amount = input.nextDouble();
+		if(amount > 0){
+			setBalance(balance + amount);
+			System.out.println("Deposited: " + getBalanceString());
+		}
+		else{
+			System.out.println("invalid amount");
+		}
+	}
+
+	public void makeWithdraw(Scanner input){
+		System.out.println("How much would you like to withdraw? ");
+		double amount = input.nextDouble();
+		if(amount <= 0){
+			System.out.println("Invalid amount. Must be higher then zero!");
+			return;
+		}
+		if(amount > balance){
+			System.out.println("Inavlid. Your current funds are: " + getBalanceString());
+			return;
+		}
+
+		setBalance(balance - amount);
+		System.out.println("Withdrew: " + getBalanceString());
 	}
 }
 
